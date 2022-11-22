4fab78d52436f3562e8881d3a0882d65f7d57369
Petrut <ivanoiu.petrut27@yahoo.com>
2022-11-22 12:46:38 +0200

    feat: add song play by second gotten in API fetc

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const songArtist = document.querySelector(".song__artist");
 
 const musicSource = document.querySelector("#music-source");
 
-// musicSource.src = `${url}currently-playing`;
-console.log(musicSource);
+const song = new Audio("http://localhost:8080/song/currently-playing");
 
 const getSongDetailsJSON = async () => {
   const response = await fetch(`${url}currently-playing/details`);
@@ -20,8 +19,36 @@ const getSong = async () => {
   console.log(response);
 };
 
+let songCurrentSecond;
+
 getSongDetailsJSON().then((songDetails) => {
-  const { title, artist } = songDetails;
+  const { title, artist, currentSecond, duration } = songDetails;
   songTitle.innerHTML = title;
   songArtist.innerHTML = artist;
+  songCurrentSecond = currentSecond;
+  document.querySelector(".audio__end").innerHTML = secondsToMinutes(duration);
 });
+
+document.querySelector(".audio__play").addEventListener("click", () => {
+  if (songCurrentSecond) {
+    song.currentTime = songCurrentSecond;
+    console.log(songCurrentSecond, "start second");
+    song.play();
+  } else {
+    // * implement HTML declaration of this message
+    console.log("Song hasn't fully loaded yet");
+  }
+});
+
+const secondsToMinutes = (seconds) => {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${minutes}:${remainingSeconds}`;
+};
+
+const nextTick = () => {
+  songCurrentSecond++;
+  console.log(songCurrentSecond);
+};
+
+setInterval(nextTick, 1000);
